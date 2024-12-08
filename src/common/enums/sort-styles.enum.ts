4112c3bd15e62219f2897e43bb40cf8e6989e924
seosau <seosausau@gmail.com>
2024-12-08 22:06:43 +0700

    update: enum SortStyles

--- src/common/enums/sort-styles.enum.ts
+++ src/common/enums/sort-styles.enum.ts
@@ -1,6 +1,6 @@
 export enum SortStyles {
-    NAME_ASC = 'name_asc',
-    NAME_DESC = 'name_desc',
-    DATE_ASC = 'date_asc',
-    DATE_DESC = 'date_desc',
+    NAME_ASC = 'NAME_ASC',
+    NAME_DESC = 'NAME_DESC',
+    DATE_ASC = 'DATE_ASC',
+    DATE_DESC = 'DATE_DESC',
 }
